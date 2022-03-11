b7f9c7442c8c3afd98e608ae07fc36ef1cfe924d
Andy Ruda <50315961+AndyRuda@users.noreply.github.com>
2022-03-11 15:35:58 -0500

    Post req

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/", methods=['POST'])
+def hello_world_POST():
+    name = request.form['name']
+    lastName = request.form['lastName']
+    return f"<p>Hello, {name} {lastName} World!</p>" 
+
